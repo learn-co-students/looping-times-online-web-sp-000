7de4d0cb6c1283f28b4d1c109e235c4049698c10
kevin lang <langkevina@gmail.com>
2019-05-19 19:22:48 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,12 @@
 def using_times
 	#your code here
+  7.times do
+    puts "Wingardium Leviosa"    
+  end
 end
 
-
-
-
+# 3.times do
+#   puts "Hiding 10 stolen jewels."
+#   jewels_in_bag = jewels_in_bag - 10
+#   puts "Now there are only #{jewels_in_bag} jewels left to hide!"
+# end
