18cc80bfb68fef1e981e65e8080ba6d53559d65c
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-08-29 21:33:04 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,5 +1,9 @@
-def using_times
-	7.times do
-    puts  "Wingardium Leviosa"
-  end
+def using_times(times)
+  times = 1
+until times == 7
+  times+= 1
 end
+puts  "Wingardium Leviosa"
+end
+
+using_times(times)
